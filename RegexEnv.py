c9837c152be838b3686b9b74755a130d1ef4bb29
Karline <karlinex98@gmail.com>
2021-08-13 10:16:30 +0200

    Gym env development progress

--- RegexEnv.py
+++ RegexEnv.py
@@ -1,55 +1,53 @@
 import csv
 import re
 import gym
+import numpy as np
+import enchant
 
-class AdresesEnv(gym.Env):
-    def __init__(self):
-        self.symbols = ['A', 'Ā', 'B', 'C', 'Č', 'D', 'E', 'Ē', 'F', 'G', 'Ģ',  'H', 'I', 'Ī', 'J', 'K', 'Ķ', 'L', 'Ļ', 'M',
-                        'N', 'Ņ', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'Ū', 'V', 'Z', 'Ž', 'a', 'ā', 'b', 'c', 'č', 'd', 'e', 'ē',
-                        'f', 'g', 'ģ', 'h', 'i', 'ī', 'j', 'k', 'ķ', 'l', 'ļ', 'm', 'n', 'ņ', 'o', 'p', 'r', 's', 'š', 't', 'u',
-                        'ū', 'v', 'z', 'ž', '-', '_', ',', '.', ';', ':', '!', '?', '*', '@', '0', '1', '2', '3', '4', '5', '6',
-                        '7', '8', '9']
+class RegexEnv(gym.Env):
+
+    filename = 'latvia.csv'
+    pattern = "(((\d+)?\.?\s?[a-zA-ZĀāČčĒēĢģĻļŅņŠšŪūŽžĪīĶķ])\s?)+"
+
+    # RegexGenerator + +
+    # "(?<= )[^_][a-p]*+[^_][r-v][^_][^_][^ ][a-p]*+\w[^_][a-p]*+ \d++(?= )|(?<= )[^ ][^ ][^ ]\w\w\w[ \d\w][^>]\w*+ [^ ]*+ \w\d++\w[^ ]*+"
+    # "(?<= )[A-Za-z][^,][^ ][^iela ][^,][^,][^/][^ ][a-p]*+[^ ] iela [^<]\d++(?= )"
+    # "(?<= )[^_][a-p]*+[^_][r-v][^_][^/][^ ][a-p]*+\w[^_][a-p]*+ \d++(?= )"
+
+    def __init__(self, text_length):
+        self.text_length = text_length
         self.address_text = ""
-        self.previous_address = self.address_text
-        self.action_space = gym.spaces.Discrete(86)
-        self.observation_space = gym.spaces.Discrete(2)
+        self.action_state = np.full(text_length, -1)
+        self.action_space = gym.spaces.Discrete(123)
+        self.observation_space = gym.spaces.Box(low=-1, high=122, shape=(text_length,), dtype=np.int64)
 
+    # Reward method: validates the generated address and returns a reward.
     def validateAddress(self, address_text):
-        filename = 'latvia.csv'
-        pattern = "(((\d+)?\.?\s?[a-zA-ZĀāČčĒēĢģĻļŅņŠšŪūŽžĪīĶķ])\s?)+"
-
-        if address_text == self.previous_address:
-            return 0
-
-        if not bool(re.match(pattern, address_text)):
+        if not bool(re.match(self.pattern, address_text)): # if address does not match the Regex pattern
             return 1
 
-        with open(filename, mode="r", encoding="utf-8-sig") as csvFile:
+        # Compare the generated address with addresses from CSV file
+        with open(self.filename, mode="r", encoding="utf-8-sig") as csvFile:
             dataReader = csv.reader(csvFile)
             for row in dataReader:
-                isValid = bool(re.match(pattern, row[0]))
-                if not isValid:
-                    return 1
+                levenshtein_distance = enchant.utils.levenshtein(row[0], address_text)
+                # TODO: Need to implement point system for the levenshtein distance
+                # Average value from all distances ?
 
         return 0
 
     def step(self, action):
-        self.address_text += self.symbols[action]
+        self.address_text += chr(action) # Append the symbol to text (corresponding ASCII table character to the action int)
+        self.action_state[len(self.address_text) - 1] = action
 
-        state = 1
+        reward = self.validateAddress(self.address_text)
 
-        if action == 2:
-            reward = 1
-        else:
-            reward = -1
+        done = (reward == 1)
 
-        done = True
-        info = {}
-        return state, reward, done, info
+        return self.action_state, reward, done, {}
 
 
     def reset(self):
-        self.previous_address = self.address_text
         self.address_text = ""
-        state = 0
-        return state
+        self.action_state = np.full((1, self.text_length), -1)
+        return self.action_state
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import csv
-import re
+from RegexEnv import RegexEnv
 
-filename = 'latvia.csv'
-pattern = "(((\d+)?\.?\s?[a-zA-ZĀāČčĒēĢģĻļŅņŠšŪūŽžĪīĶķ])\s?)+"
-
-with open(filename, mode="r", encoding="utf-8-sig") as csvFile:
-    dataReader = csv.reader(csvFile)
-    for row in dataReader:
-        isValid = bool(re.match(pattern, row[0]))
-        if not isValid:
-            print(row[0])
-
-print("Done.")
+text_length = 30
+env = RegexEnv(text_length)
+state_shape = env.observation_space.shape
+for t in range(text_length):
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
+env.close()
